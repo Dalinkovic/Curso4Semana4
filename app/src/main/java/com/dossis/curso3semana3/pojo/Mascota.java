0bddc0ee9fbd73b5b1ed82d284994e95d134fdba
Dalinkovic <daniel.catala.paches@gmail.com>
2021-04-02 12:31:09 +0200

    Initial commit

---
+++ app/src/main/java/com/dossis/curso3semana3/pojo/Mascota.java
@@ -0,0 +1,10 @@
+package com.dossis.curso3semana3.pojo;
+
+import android.net.Uri;
+
+public class Mascota {
+    private int id;
+    private String nombre;
+    private int likes;
+    private Uri uri;
+}
